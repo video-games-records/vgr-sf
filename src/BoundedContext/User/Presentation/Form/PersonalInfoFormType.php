8c16ba275596972b893b77d6dff6ea747f67ae3a
magicbart <magicbart@gmail.com>
2026-01-24 18:35:14 +0100

    Add personal infos form

---
+++ src/BoundedContext/User/Presentation/Form/PersonalInfoFormType.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\BoundedContext\User\Presentation\Form;
+
+use App\BoundedContext\User\Domain\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @extends AbstractType<array<string, mixed>>
+ */
+class PersonalInfoFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'profile.personal_info.form.username.label',
+                'constraints' => [
+                    new Assert\NotBlank(
+                        message: 'profile.personal_info.form.error.username_required',
+                    ),
+                    new Assert\Length(
+                        min: 3,
+                        max: 100,
+                        minMessage: 'profile.personal_info.form.error.username_too_short',
+                        maxMessage: 'profile.personal_info.form.error.username_too_long',
+                    ),
+                    new Assert\Regex(
+                        pattern: '/^[a-zA-Z0-9_-]+$/',
+                        message: 'profile.personal_info.form.error.username_invalid_format',
+                    ),
+                ],
+                'attr' => [
+                    'placeholder' => 'profile.personal_info.form.username.placeholder',
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'profile.personal_info.form.email.label',
+                'constraints' => [
+                    new Assert\NotBlank(
+                        message: 'profile.personal_info.form.error.email_required',
+                    ),
+                    new Assert\Email(
+                        message: 'profile.personal_info.form.error.email_invalid',
+                    ),
+                ],
+                'attr' => [
+                    'placeholder' => 'profile.personal_info.form.email.placeholder',
+                ],
+            ])
+            ->add('language', ChoiceType::class, [
+                'label' => 'profile.personal_info.form.language.label',
+                'choices' => [
+                    'English' => 'en',
+                    'Français' => 'fr',
+                ],
+                'attr' => [
+                    'class' => 'form-select',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'profile.personal_info.form.submit',
+                'attr' => [
+                    'class' => 'btn btn-primary',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'csrf_token_id' => 'personal_info',
+            'translation_domain' => 'User',
+        ]);
+    }
+}
--- src/BoundedContext/User/Presentation/Web/Controller/Profile/PersonalInfo.php
+++ src/BoundedContext/User/Presentation/Web/Controller/Profile/PersonalInfo.php
@@ -4,20 +4,44 @@ declare(strict_types=1);
 
 namespace App\BoundedContext\User\Presentation\Web\Controller\Profile;
 
+use App\BoundedContext\User\Domain\Entity\User;
+use App\BoundedContext\User\Presentation\Form\PersonalInfoFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[AsController]
 class PersonalInfo extends AbstractController
 {
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly TranslatorInterface $translator,
+    ) {
+    }
+
     #[Route('/profile/personal-info', name: 'app_profile_personal_info', methods: ['GET', 'POST'])]
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
+        /** @var User $user */
         $user = $this->getUser();
 
-        // TODO: Implement personal information form
-        return $this->render('@User/profile/personal_info.html.twig');
+        $form = $this->createForm(PersonalInfoFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+
+            $this->addFlash('success', $this->translator->trans('profile.personal_info.message.success', [], 'User'));
+
+            return $this->redirectToRoute('app_profile_personal_info');
+        }
+
+        return $this->render('@User/profile/personal_info.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
