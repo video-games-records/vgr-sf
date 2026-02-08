b5051c758c98d676d078ceca739fbac11a04ce63
magicbart <magicbart@gmail.com>
2026-02-08 07:14:02 +0100

    Add home leaderboard into cache

--- public/index.php
+++ public/index.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+
+    if (in_array($context['APP_ENV'], ['prod', 'staging'], true)) {
+        $kernel = new HttpCache($kernel);
+    }
+
+    return $kernel;
 };
--- src/BoundedContext/VideoGamesRecords/Core/Presentation/Web/Controller/Ranking/TopPlayersController.php
+++ src/BoundedContext/VideoGamesRecords/Core/Presentation/Web/Controller/Ranking/TopPlayersController.php
@@ -21,7 +21,7 @@ class TopPlayersController extends AbstractController
     /**
      * @throws ORMException
      */
-    public function gamePoints(?int $teamId = null, int $limit = 5, bool $showNbGame = false): Response
+    public function gamePoints(?int $teamId = null, int $limit = 5, bool $showNbGame = false, int $ttl = 0): Response
     {
         $topPlayers = $this->playerRankingProvider->getRankingPointGame([
             'maxRank' => $limit,
@@ -29,16 +29,23 @@ class TopPlayersController extends AbstractController
             'idTeam' => $teamId
         ]);
 
-        return $this->render('@VideoGamesRecordsCore/ranking/_top_players_game_points.html.twig', [
+        $response = $this->render('@VideoGamesRecordsCore/ranking/_top_players_game_points.html.twig', [
             'players' => $topPlayers,
             'showNbGame' => $showNbGame
         ]);
+
+        if ($ttl > 0) {
+            $response->setPublic();
+            $response->setSharedMaxAge($ttl);
+        }
+
+        return $response;
     }
 
     /**
      * @throws ORMException
      */
-    public function cups(?int $teamId = null, int $limit = 5): Response
+    public function cups(?int $teamId = null, int $limit = 5, int $ttl = 0): Response
     {
         $topPlayers = $this->playerRankingProvider->getRankingCup([
             'maxRank' => $limit,
@@ -46,15 +53,22 @@ class TopPlayersController extends AbstractController
             'idTeam' => $teamId
         ]);
 
-        return $this->render('@VideoGamesRecordsCore/ranking/_top_players_cups.html.twig', [
+        $response = $this->render('@VideoGamesRecordsCore/ranking/_top_players_cups.html.twig', [
             'players' => $topPlayers
         ]);
+
+        if ($ttl > 0) {
+            $response->setPublic();
+            $response->setSharedMaxAge($ttl);
+        }
+
+        return $response;
     }
 
     /**
      * @throws ORMException
      */
-    public function medals(?int $teamId = null, int $limit = 5): Response
+    public function medals(?int $teamId = null, int $limit = 5, int $ttl = 0): Response
     {
         $topPlayers = $this->playerRankingProvider->getRankingMedals([
             'maxRank' => $limit,
@@ -62,15 +76,22 @@ class TopPlayersController extends AbstractController
             'idTeam' => $teamId
         ]);
 
-        return $this->render('@VideoGamesRecordsCore/ranking/_top_players_medals.html.twig', [
+        $response = $this->render('@VideoGamesRecordsCore/ranking/_top_players_medals.html.twig', [
             'players' => $topPlayers
         ]);
+
+        if ($ttl > 0) {
+            $response->setPublic();
+            $response->setSharedMaxAge($ttl);
+        }
+
+        return $response;
     }
 
     /**
      * @throws ORMException
      */
-    public function recordPoints(?int $teamId = null, int $limit = 5, bool $showNbChart = false): Response
+    public function recordPoints(?int $teamId = null, int $limit = 5, bool $showNbChart = false, int $ttl = 0): Response
     {
         $topPlayers = $this->playerRankingProvider->getRankingPointChart([
             'maxRank' => $limit,
@@ -78,16 +99,23 @@ class TopPlayersController extends AbstractController
             'idTeam' => $teamId
         ]);
 
-        return $this->render('@VideoGamesRecordsCore/ranking/_top_players_record_points.html.twig', [
+        $response = $this->render('@VideoGamesRecordsCore/ranking/_top_players_record_points.html.twig', [
             'players' => $topPlayers,
             'showNbChart' => $showNbChart
         ]);
+
+        if ($ttl > 0) {
+            $response->setPublic();
+            $response->setSharedMaxAge($ttl);
+        }
+
+        return $response;
     }
 
     /**
      * @throws ORMException
      */
-    public function badges(?int $teamId = null, int $limit = 5): Response
+    public function badges(?int $teamId = null, int $limit = 5, int $ttl = 0): Response
     {
         $topPlayers = $this->playerRankingProvider->getRankingBadge([
             'maxRank' => $limit,
@@ -95,15 +123,22 @@ class TopPlayersController extends AbstractController
             'idTeam' => $teamId
         ]);
 
-        return $this->render('@VideoGamesRecordsCore/ranking/_top_players_badges.html.twig', [
+        $response = $this->render('@VideoGamesRecordsCore/ranking/_top_players_badges.html.twig', [
             'players' => $topPlayers
         ]);
+
+        if ($ttl > 0) {
+            $response->setPublic();
+            $response->setSharedMaxAge($ttl);
+        }
+
+        return $response;
     }
 
     /**
      * @throws ORMException
      */
-    public function proofs(?int $teamId = null, int $limit = 5, bool $showPercentage = false): Response
+    public function proofs(?int $teamId = null, int $limit = 5, bool $showPercentage = false, int $ttl = 0): Response
     {
         $topPlayers = $this->playerRankingProvider->getRankingProof([
             'maxRank' => $limit,
@@ -111,9 +146,16 @@ class TopPlayersController extends AbstractController
             'idTeam' => $teamId
         ]);
 
-        return $this->render('@VideoGamesRecordsCore/ranking/_top_players_proofs.html.twig', [
+        $response = $this->render('@VideoGamesRecordsCore/ranking/_top_players_proofs.html.twig', [
             'players' => $topPlayers,
             'showPercentage' => $showPercentage
         ]);
+
+        if ($ttl > 0) {
+            $response->setPublic();
+            $response->setSharedMaxAge($ttl);
+        }
+
+        return $response;
     }
 }
