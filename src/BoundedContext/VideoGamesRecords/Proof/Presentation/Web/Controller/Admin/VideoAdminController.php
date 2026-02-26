1f428bd45df095fd5a6592cc44cfa8fff04085a7
magicbart <magicbart@gmail.com>
2026-02-26 20:46:45 +0100

    Activate YoutubeDataHandler

--- src/BoundedContext/VideoGamesRecords/Proof/Presentation/Web/Controller/Admin/VideoAdminController.php
+++ src/BoundedContext/VideoGamesRecords/Proof/Presentation/Web/Controller/Admin/VideoAdminController.php
@@ -7,19 +7,18 @@ namespace App\BoundedContext\VideoGamesRecords\Proof\Presentation\Web\Controller
 use App\BoundedContext\VideoGamesRecords\Proof\Domain\Entity\Video;
 use App\SharedKernel\Presentation\Web\Controller\Admin\AbstractCRUDController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-
-//use App\BoundedContext\VideoGamesRecords\Proof\Application\Handler\YoutubeDataHandler;
+use App\BoundedContext\VideoGamesRecords\Proof\Application\Handler\YoutubeDataHandler;
 
 /**
  * @extends AbstractCRUDController<Video>
  */
 class VideoAdminController extends AbstractCRUDController
 {
-    //private YoutubeDataHandler $youtubeDataHandler;
+    private YoutubeDataHandler $youtubeDataHandler;
 
-    public function __construct(/* YoutubeDataHandler $youtubeDataHandler */)
+    public function __construct(YoutubeDataHandler $youtubeDataHandler)
     {
-        //$this->youtubeDataHandler = $youtubeDataHandler;
+        $this->youtubeDataHandler = $youtubeDataHandler;
     }
 
 
@@ -29,7 +28,7 @@ class VideoAdminController extends AbstractCRUDController
      */
     public function majAction(int $id): RedirectResponse
     {
-        //$this->youtubeDataHandler->process($this->admin->getSubject());
+        $this->youtubeDataHandler->process($this->admin->getSubject());
         $this->addFlash('sonata_flash_success', 'Video data maj successfully - Handler to be implemented');
         return new RedirectResponse($this->admin->generateUrl('list'));
     }
