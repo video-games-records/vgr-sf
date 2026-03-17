0a08f86d2dff9fd3885216270d62b3a36614c288
magicbart <magicbart@gmail.com>
2026-03-17 18:34:17 +0100

    Display icone picture/video on leaderboard

--- src/BoundedContext/VideoGamesRecords/Shared/Presentation/Twig/Components/PlayerChartStatus.php
+++ src/BoundedContext/VideoGamesRecords/Shared/Presentation/Twig/Components/PlayerChartStatus.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace App\BoundedContext\VideoGamesRecords\Shared\Presentation\Twig\Components;
 
 use App\BoundedContext\VideoGamesRecords\Core\Domain\ValueObject\PlayerChartStatusEnum;
+use App\BoundedContext\VideoGamesRecords\Proof\Domain\Entity\Proof;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent('player_chart_status')]
 class PlayerChartStatus
 {
     public ?PlayerChartStatusEnum $status = null;
+    public ?Proof $proof = null;
 
     public function getCssClass(): string
     {
