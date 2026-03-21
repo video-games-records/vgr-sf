11466919d765ef75fd8a1b3db1b56f9f4ab1ebbc
magicbart <magicbart@gmail.com>
2026-03-21 22:14:50 +0100

    Fix load badge picture

--- src/BoundedContext/VideoGamesRecords/Badge/Presentation/Web/Controller/GetPicture.php
+++ src/BoundedContext/VideoGamesRecords/Badge/Presentation/Web/Controller/GetPicture.php
@@ -6,8 +6,8 @@ namespace App\BoundedContext\VideoGamesRecords\Badge\Presentation\Web\Controller
 
 use League\Flysystem\FilesystemOperator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\StreamedResponse;
-use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
 use App\BoundedContext\VideoGamesRecords\Badge\Domain\Entity\Badge;
 use App\BoundedContext\VideoGamesRecords\Badge\Domain\ValueObject\BadgeType;
@@ -27,19 +27,37 @@ class GetPicture extends AbstractController
         requirements: ['id' => '[1-9]\d*'],
         methods: ['GET']
     )]
-    #[Cache(maxage: 3600 * 24, public: true, mustRevalidate: true)]
-    public function __invoke(Badge $badge): StreamedResponse
+    public function __invoke(Badge $badge, Request $request): StreamedResponse
     {
         $path = $badge->getType()->getDirectory() . DIRECTORY_SEPARATOR . $badge->getPicture();
         if (!$this->appStorage->fileExists($path)) {
             $path = BadgeType::getDefaultDirectory() . DIRECTORY_SEPARATOR . 'default.gif';
         }
 
+        $lastModified = $this->appStorage->lastModified($path);
+        $etag = '"' . md5($path . $lastModified) . '"';
+
+        $response = new StreamedResponse();
+        $response->headers->set('Content-Type', 'image/gif');
+        $response->headers->set('Cache-Control', 'no-cache');
+        $response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s', $lastModified) . ' GMT');
+        $response->headers->set('ETag', $etag);
+
+        $ifNoneMatch = $request->headers->get('If-None-Match');
+        $ifModifiedSince = $request->headers->get('If-Modified-Since');
+        $notModified = ($ifNoneMatch !== null && $ifNoneMatch === $etag)
+            || ($ifNoneMatch === null && $ifModifiedSince !== null && strtotime($ifModifiedSince) >= $lastModified);
+
+        if ($notModified) {
+            $response->setStatusCode(304);
+            return $response;
+        }
+
         $stream = $this->appStorage->readStream($path);
-        $response = new StreamedResponse(function () use ($stream) {
+        $response->setCallback(function () use ($stream) {
             fpassthru($stream);
-        }, 200, ['Content-Type' => 'image/gif']);
-        $response->headers->set('Cache-Control', 'public, max-age=86400');
+        });
+
         return $response;
     }
 }
