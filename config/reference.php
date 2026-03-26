b1a465fae1f8e0e4f57bc9a094e733090f828b28
magicbart <magicbart@gmail.com>
2026-03-26 22:30:08 +0100

    Revert "Add symfony ux native"
    This reverts commit eeeda0a902d29bc6ff27529c15d1337f88cce503.

--- config/reference.php
+++ config/reference.php
@@ -2404,9 +2404,6 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     extra_data_provider?: scalar|Param|null, // Default: null
  *     providers?: array<string, mixed>,
  * }
- * @psalm-type UxNativeConfig = array{
- *     output_dir?: scalar|Param|null, // Directory where configuration JSON files are written. Defaults to %kernel.project_dir%/public. // Default: null
- * }
  * @psalm-type ConfigType = array{
  *     imports?: ImportsConfig,
  *     parameters?: ParametersConfig,
@@ -2442,7 +2439,6 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     fd_log_viewer?: FdLogViewerConfig,
  *     nelmio_cors?: NelmioCorsConfig,
  *     dh_auditor?: DhAuditorConfig,
- *     ux_native?: UxNativeConfig,
  *     "when@dev"?: array{
  *         imports?: ImportsConfig,
  *         parameters?: ParametersConfig,
@@ -2482,7 +2478,6 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         fd_log_viewer?: FdLogViewerConfig,
  *         nelmio_cors?: NelmioCorsConfig,
  *         dh_auditor?: DhAuditorConfig,
- *         ux_native?: UxNativeConfig,
  *     },
  *     "when@prod"?: array{
  *         imports?: ImportsConfig,
@@ -2519,7 +2514,6 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         fd_log_viewer?: FdLogViewerConfig,
  *         nelmio_cors?: NelmioCorsConfig,
  *         dh_auditor?: DhAuditorConfig,
- *         ux_native?: UxNativeConfig,
  *     },
  *     "when@test"?: array{
  *         imports?: ImportsConfig,
@@ -2559,7 +2553,6 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         dama_doctrine_test?: DamaDoctrineTestConfig,
  *         nelmio_cors?: NelmioCorsConfig,
  *         dh_auditor?: DhAuditorConfig,
- *         ux_native?: UxNativeConfig,
  *     },
  *     ...<string, ExtensionType|array{ // extra keys must follow the when@%env% pattern or match an extension alias
  *         imports?: ImportsConfig,
