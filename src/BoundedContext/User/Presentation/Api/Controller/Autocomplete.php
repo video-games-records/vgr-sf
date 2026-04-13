6c42ac26673863e0c2543bff1c0569491d3214fd
David Benard <david.benard@myauditpilot.com>
2026-04-13 16:59:52 +0200

    Fix user Autocomplete

--- src/BoundedContext/User/Presentation/Api/Controller/Autocomplete.php
+++ src/BoundedContext/User/Presentation/Api/Controller/Autocomplete.php
@@ -9,9 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_USER')]
 class Autocomplete extends AbstractController
 {
     public function __construct(
